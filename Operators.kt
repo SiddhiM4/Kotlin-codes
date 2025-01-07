fd71ffe358e8865ed45c39bde3f7efb1dd235b79
SiddhiM4 <siddhimandekar47@gmail.com>
2025-01-07 22:40:03 +0530

    variables and operators

---
+++ Operators.kt
@@ -0,0 +1,25 @@
+fun main() {
+//    var i = 13
+//    var j = 2
+
+//    println(i + j)
+//    println(i - j)
+//    println(i.toFloat() / j)
+//    println(i * j)
+//    println(i % j)
+
+    //relational
+//    println(i > j)
+//    println(i < j)
+//    println(i >= j)
+//    println(i <= j)
+//    println(i == j)
+//    println(i != j)
+
+   //pre and post increment
+
+    var i = 10
+    --i
+    i++
+    println(i)
+}
---
+++ Variables.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var score : Int = 5;
+    score = 10;
+    println(score);
+
+    val message : String = "Hello World!";
+    //message = "hi";
+    println(message);
+}
